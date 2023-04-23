f7fd55cf756f7158341aeefeca28d2c1c6e625bf
Juliann Stainbrook <hyperjoule@gmail.com>
2023-04-23 19:46:09 -0400

    linter fix

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import { StyleSheet, View } from 'react-native';
-import ChatGPT from './src';
+/* eslint-disable react/react-in-jsx-scope */
+import { StyleSheet, View } from 'react-native'
+import ChatGPT from './src'
 
-export default function App() {
+export default function App () {
   return (
     <View style={styles.container}>
       <ChatGPT/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-});
+    flex: 1
+  }
+})
