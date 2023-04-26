cdc51b5bd61d7be8467f72d0f23f2a183954fb32
Juliann Stainbrook <hyperjoule@gmail.com>
2023-04-25 20:20:11 -0400

    Test publish api_key this way

--- src/api/config.js
+++ src/api/config.js
@@ -1,14 +1,7 @@
 import Constants from 'expo-constants'
-import { Platform } from 'react-native'
 
-let API_KEY
+const API_KEY = (process.env.API_KEY === 'undefined') ? Constants.manifest.extra.API_KEY : Constants.manifest.extra.API_KEY
 
-if (Platform.OS === 'web') {
-  // For the Heroku deployment (web version)
-  API_KEY = process.env.API_KEY
-} else if (Constants?.manifest?.extra) {
-  // For the mobile app (Expo)
-  API_KEY = Constants.manifest.extra.API_KEY
-}
+console.log(API_KEY)
 
 export { API_KEY }
