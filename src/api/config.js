6898d82e38f08941786a41eb2b5fa951e1a2dfcc
Juliann Stainbrook <hyperjoule@gmail.com>
2023-04-25 20:09:40 -0400

    Push heroku changes

--- src/api/config.js
+++ src/api/config.js
@@ -1,15 +1,14 @@
 import Constants from 'expo-constants'
+import { Platform } from 'react-native'
 
 let API_KEY
 
-console.log(Constants.manifest.extra)
-console.log(process.env.API_KEY)
-if (Constants?.manifest?.extra) {
-  // For the mobile app (Expo)
-  API_KEY = Constants.manifest.extra.API_KEY
-} else {
+if (Platform.OS === 'web') {
   // For the Heroku deployment (web version)
   API_KEY = process.env.API_KEY
+} else if (Constants?.manifest?.extra) {
+  // For the mobile app (Expo)
+  API_KEY = Constants.manifest.extra.API_KEY
 }
 
 export { API_KEY }
